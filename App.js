2ce0e3e91a78e67aeab4d1877dc2656391798eb4
Molimat <matheusmolin@poli.ufrj.br>
2018-10-26 00:04:50 -0300

    Inicio

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Open up App.js to start working on your app!!!</Text>
+        <Header title = "Pessoas" />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
