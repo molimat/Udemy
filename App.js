91b93480c3ad9a99b5dc00fa5ea9b163ce520ab6
Molimat <matheusmolin@poli.ufrj.br>
2018-10-29 20:04:33 -0300

    Adicionado arquivo people list

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
 import Header from './src/components/Header';
+//import PeopleList from './src/components/PeopleList';
+
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      peoples: []
+    };
+
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?nat=br&results=5')
+    .then (response => {
+       const {results} = response.data;
+       this.setState({
+         peoples: results
+       });
+    })
+  }
+
+
+
+ renderList2() {
+  const textElements = this.state.peoples.map(people => <Text key = {people.name.first}>{people.name.first}</Text>);
+  return textElements;
+}
+
   render() {
     return (
       <View>
         <Text>Open up App.js to start working on your app!!!</Text>
-        <Header title = "Pessoas2" />
+        <Header title = "Pessoas" />
+        {this.renderList2()}
       </View>
     );
   }
 }
+
